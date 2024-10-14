5203651f8675e245e5bde6647070789bc2d3b045
Maxim Matyunin <m.a.matyunin@tinkoff.ru>
2024-10-14 15:51:03 +0400

    refactor(example-app): migrate ng/core to 15

--- example-app/src/app/app.module.ts
+++ example-app/src/app/app.module.ts
@@ -1,26 +1,27 @@
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
-import { EffectsModule } from '@ngrx/effects';
-import { RouterStateSerializer, StoreRouterConnectingModule } from '@ngrx/router-store';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RouterModule } from "@angular/router";
+import { EffectsModule } from "@ngrx/effects";
+import {
+  RouterStateSerializer,
+  StoreRouterConnectingModule,
+} from "@ngrx/router-store";
+import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
-import { environment } from '../environments/environment';
-import { AppComponent } from './app.component';
-import { reducers } from './app.reducer';
-import { routes } from './app.routes';
-import { LayoutModule } from './layout/layout.module';
-import { MaterialModule } from './material';
-import { SharedModule } from './shared/shared.module';
-import { CustomRouterStateSerializer } from './shared/utils';
+import { environment } from "../environments/environment";
+import { AppComponent } from "./app.component";
+import { reducers } from "./app.reducer";
+import { routes } from "./app.routes";
+import { LayoutModule } from "./layout/layout.module";
+import { MaterialModule } from "./material";
+import { SharedModule } from "./shared/shared.module";
+import { CustomRouterStateSerializer } from "./shared/utils";
 
-export const COMPONENTS = [
-  AppComponent,
-];
+export const COMPONENTS = [AppComponent];
 
 @NgModule({
   imports: [
@@ -30,7 +31,7 @@ export const COMPONENTS = [
     MaterialModule,
     SharedModule,
     HttpClientModule,
-    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
+    RouterModule.forRoot(routes),
     StoreModule.forRoot(reducers),
     StoreRouterConnectingModule.forRoot(),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
@@ -44,4 +45,4 @@ export const COMPONENTS = [
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
