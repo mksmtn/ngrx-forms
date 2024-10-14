aeb0950aee2b0f677ba9db1d6df54e198da16a9e
Maxim Matyunin <m.a.matyunin@tinkoff.ru>
2024-10-14 15:47:22 +0400

    refactor(example-app): use new features

--- example-app/src/app/app.routes.ts
+++ example-app/src/app/app.routes.ts
@@ -1,58 +1,96 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { provideState } from "@ngrx/store";
+import { reducer as arrayReducer } from "./array/array.reducer";
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/introduction', pathMatch: 'full' },
+  { path: "", redirectTo: "/introduction", pathMatch: "full" },
   {
-    path: 'introduction',
-    loadChildren: () => import('./introduction/introduction.module').then(m => m.IntroductionModule),
+    path: "introduction",
+    loadChildren: () =>
+      import("./introduction/introduction.module").then(
+        (m) => m.IntroductionModule
+      ),
   },
   {
-    path: 'simpleForm',
-    loadChildren: () => import('./simple-form/simple-form.module').then(m => m.SimpleFormModule),
+    path: "simpleForm",
+    loadChildren: () =>
+      import("./simple-form/simple-form.module").then(
+        (m) => m.SimpleFormModule
+      ),
   },
   {
-    path: 'simpleFormNgrx8',
-    loadChildren: () => import('./simple-form-ngrx8/simple-form-ngrx8.module').then(m => m.SimpleFormNgrx8Module),
+    path: "simpleFormNgrx8",
+    loadChildren: () =>
+      import("./simple-form-ngrx8/simple-form-ngrx8.module").then(
+        (m) => m.SimpleFormNgrx8Module
+      ),
   },
   {
-    path: 'syncValidation',
-    loadChildren: () => import('./sync-validation/sync-validation.module').then(m => m.SyncValidationModule),
+    path: "syncValidation",
+    loadChildren: () =>
+      import("./sync-validation/sync-validation.module").then(
+        (m) => m.SyncValidationModule
+      ),
   },
   {
-    path: 'asyncValidation',
-    loadChildren: () => import('./async-validation/async-validation.module').then(m => m.AsyncValidationModule),
+    path: "asyncValidation",
+    loadChildren: () =>
+      import("./async-validation/async-validation.module").then(
+        (m) => m.AsyncValidationModule
+      ),
   },
   {
-    path: 'array',
-    loadChildren: () => import('./array/array.module').then(m => m.ArrayModule),
+    path: "array",
+    loadComponent: () =>
+      import("./array/array.component").then((m) => m.ArrayPageComponent),
+    providers: [provideState("array", arrayReducer)],
   },
   {
-    path: 'dynamic',
-    loadChildren: () => import('./dynamic/dynamic.module').then(m => m.DynamicModule),
+    path: "dynamic",
+    loadChildren: () =>
+      import("./dynamic/dynamic.module").then((m) => m.DynamicModule),
   },
   {
-    path: 'valueBoxing',
-    loadChildren: () => import('./value-boxing/value-boxing.module').then(m => m.ValueBoxingModule),
+    path: "valueBoxing",
+    loadChildren: () =>
+      import("./value-boxing/value-boxing.module").then(
+        (m) => m.ValueBoxingModule
+      ),
   },
   {
-    path: 'valueConversion',
-    loadChildren: () => import('./value-conversion/value-conversion.module').then(m => m.ValueConversionModule),
+    path: "valueConversion",
+    loadChildren: () =>
+      import("./value-conversion/value-conversion.module").then(
+        (m) => m.ValueConversionModule
+      ),
   },
   {
-    path: 'recursiveUpdate',
-    loadChildren: () => import('./recursive-update/recursive-update.module').then(m => m.RecursiveUpdateModule),
+    path: "recursiveUpdate",
+    loadChildren: () =>
+      import("./recursive-update/recursive-update.module").then(
+        (m) => m.RecursiveUpdateModule
+      ),
   },
   {
-    path: 'material',
-    loadChildren: () => import('./material-example/material.module').then(m => m.MaterialExampleModule),
+    path: "material",
+    loadChildren: () =>
+      import("./material-example/material.module").then(
+        (m) => m.MaterialExampleModule
+      ),
   },
   {
-    path: 'localStateIntroduction',
-    loadChildren: () => import('./local-state-introduction/local-state-introduction.module').then(m => m.LocalStateIntroductionModule),
+    path: "localStateIntroduction",
+    loadChildren: () =>
+      import("./local-state-introduction/local-state-introduction.module").then(
+        (m) => m.LocalStateIntroductionModule
+      ),
   },
   {
-    path: 'localStateAdvanced',
-    loadChildren: () => import('./local-state-advanced/local-state-advanced.module').then(m => m.LocalStateAdvancedModule),
+    path: "localStateAdvanced",
+    loadChildren: () =>
+      import("./local-state-advanced/local-state-advanced.module").then(
+        (m) => m.LocalStateAdvancedModule
+      ),
   },
-  { path: '**', redirectTo: '/introduction' },
+  { path: "**", redirectTo: "/introduction" },
 ];
--- example-app/src/app/array/array.component.ts
+++ example-app/src/app/array/array.component.ts
@@ -1,22 +1,22 @@
 import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
 import { select, Store } from "@ngrx/store";
-import { FormGroupState } from "ngrx-forms";
+import { FormGroupState, NgrxFormsModule } from "ngrx-forms";
 import { Observable } from "rxjs";
 
 import { FormValue, State } from "./array.reducer";
+import { MaterialModule } from "../material";
+import { SharedModule } from "../shared/shared.module";
 
 @Component({
   selector: "ngf-array",
   templateUrl: "./array.component.html",
   styleUrls: ["./array.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [MaterialModule, NgrxFormsModule, SharedModule],
 })
 export class ArrayPageComponent {
   protected readonly formState$: Observable<FormGroupState<FormValue>> = inject<
     Store<State>
   >(Store).pipe(select((s) => s.array.formState));
-
-  protected trackByIndex(index: number): number {
-    return index;
-  }
 }
